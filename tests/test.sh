e80a5130f8249eca1a005f8877a11ce572f9f500
Red_M <git@red-m.net>
2020-02-01 23:27:36 +1000

    Remove old test script.

--- tests/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATH=$PATH:~/.local/bin
-
-pip3 install --user coveralls pytest-cov paramiko > /dev/null
-py.test --cov redexpect --cov-config .coveragerc
-coverage html
-coveralls || true
