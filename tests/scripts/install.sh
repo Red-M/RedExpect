f4fcfd3c62f179c135d220ca252db29bf1a0843d
Red_M <git@red-m.net>
2021-06-18 23:14:28 +1000

    Update CICD from RedSSH.

--- tests/scripts/install.sh
+++ tests/scripts/install.sh
@@ -3,6 +3,18 @@ CI_SYSTEM=${1}
 PYTHON_MAJOR_VERSION=${2}
 PYTHON_MINOR_VERSION=${3}
 
+if [ ${CI_SYSTEM} == "TRAVIS" ]; then
+    PYTHON_VERSION=${PYTHON_MAJOR_VERSION}
+else
+    if [ ! -z $PYTHON_MAJOR_VERSION ]; then
+        PYTHON_VERSION=${PYTHON_MAJOR_VERSION}
+    fi
+
+    if [ ! -z $PYTHON_MAJOR_VERSION ] && [ ! -z $PYTHON_MINOR_VERSION ]; then
+        PYTHON_VERSION=${PYTHON_VERSION}"."${PYTHON_MINOR_VERSION}
+    fi
+fi
+
 pip${PYTHON_VERSION} install -e ./[tests]
 pip${PYTHON_VERSION} install -e ./[docs]
 pip${PYTHON_VERSION} install sftpserver
--- tests/scripts/install_root.sh
+++ tests/scripts/install_root.sh
@@ -3,14 +3,24 @@ CI_SYSTEM=${1}
 PYTHON_MAJOR_VERSION=${2}
 PYTHON_MINOR_VERSION=${3}
 
+if [ -n $CI_SYSTEM ] && [ ${CI_SYSTEM} == "GITLAB" ] && [ ! -z $(getent hosts deb-mirror | awk '{ print $1 }') ]; then
+    DEBIAN_VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | sed -E 's/.+?\=//g')
+    echo "deb http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME} main non-free contrib
+deb-src http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME} main non-free contrib
+deb http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME}-updates main contrib non-free
+deb-src http://deb-mirror/deb.debian.org/debian ${DEBIAN_VERSION_CODENAME}-updates main contrib non-free
+$(cat /etc/apt/sources.list)" > /etc/apt/sources.list
+fi
+
+
 if [ ${CI_SYSTEM} == "TRAVIS" ]; then
     PYTHON_VERSION=${PYTHON_MAJOR_VERSION}
 else
-    if [ ! -z PYTHON_MAJOR_VERSION ]; then
+    if [ ! -z $PYTHON_MAJOR_VERSION ]; then
         PYTHON_VERSION=${PYTHON_MAJOR_VERSION}
     fi
 
-    if [ ! -z PYTHON_MAJOR_VERSION ] && [ ! -z PYTHON_MINOR_VERSION ]; then
+    if [ ! -z $PYTHON_MAJOR_VERSION ] && [ ! -z $PYTHON_MINOR_VERSION ]; then
         PYTHON_VERSION=${PYTHON_VERSION}"."${PYTHON_MINOR_VERSION}
     fi
 fi
@@ -19,21 +29,20 @@ apt update
 apt install -y make curl wget openssh-client openssh-server git cmake libssl-dev zlib1g-dev
 
 # Travis takes care of all the python deps
-if [ -n CI_SYSTEM ] && [ CI_SYSTEM != "TRAVIS" ]; then
-    apt install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev
+if [ -n $CI_SYSTEM ] && [ ${CI_SYSTEM} != "TRAVIS" ]; then
+    apt install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python${PYTHON_MAJOR_VERSION}-pip
     apt install -y python${PYTHON_VERSION}-distutils || true
     wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
     python${PYTHON_VERSION} ./get-pip.py
 fi
 
-if [ -n CI_SYSTEM ] && [ CI_SYSTEM == "GITLAB" ]; then
+if [ -n $CI_SYSTEM ] && [ ${CI_SYSTEM} == "GITLAB" ]; then
     sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
     echo "export VISIBLE=now" >> /etc/profile
     source /etc/profile
     mkdir /var/run/sshd || true
-    chmod 755 /builds /builds/Red_M
 fi
 
-if [ -n CI_SYSTEM ]; then
+if [ -n $CI_SYSTEM ]; then
     mkdir /run/sshd || true
 fi
